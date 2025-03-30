295098472cbb440472b741ae7e90239524d488ff
JoaoBortoluz <joaovitorbortoluzdarosa223@gmail.com>
2025-03-30 19:34:58 -0300

    feat: add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,245 @@
+-- WezTerm Keybindings Documentation by dragonlobster 
+-- ===================================================
+-- Leader Key:
+-- The leader key is set to ALT + q, with a timeout of 2000 milliseconds (2 seconds).
+-- To execute any keybinding, press the leader key (ALT + q) first, then the corresponding key.
+
+-- Keybindings:
+-- 1. Tab Management:
+--    - LEADER + c: Create a new tab in the current pane's domain.
+--    - LEADER + x: Close the current pane (with confirmation).
+--    - LEADER + p: Switch to the previous tab.
+--    - LEADER + n: Switch to the next tab.
+--    - LEADER + <number>: Switch to a specific tab (0–9).
+
+-- 2. Pane Splitting:
+--    - LEADER + |: Split the current pane horizontally into two panes.
+--    - LEADER + -: Split the current pane vertically into two panes.
+
+-- 3. Pane Navigation:
+--    - LEADER + h: Move to the pane on the left.
+--    - LEADER + j: Move to the pane below.
+--    - LEADER + k: Move to the pane above.
+--    - LEADER + l: Move to the pane on the right.
+
+-- 4. Pane Resizing:
+--    - LEADER + LeftArrow: Increase the pane size to the left by 5 units.
+--    - LEADER + RightArrow: Increase the pane size to the right by 5 units.
+--    - LEADER + DownArrow: Increase the pane size downward by 5 units.
+--    - LEADER + UpArrow: Increase the pane size upward by 5 units.
+
+-- 5. Status Line:
+--    - The status line indicates when the leader key is active, displaying an ocean wave emoji (🌊).
+
+-- Miscellaneous Configurations:
+-- - Tabs are shown even if there's only one tab.
+-- - The tab bar is located at the bottom of the terminal window.
+-- - Tab and split indices are zero-based.
+
+
+-- Pull in the wezterm API
+local wezterm = require "wezterm"
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.enable_wayland=false
+
+-- config.front_end = "OpenGL"
+-- config.max_fps = 144
+-- config.default_cursor_style = "BlinkingBlock"
+-- config.animation_fps = 1
+-- config.cursor_blink_rate = 500
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Macchiato"
+config.font = wezterm.font("JetBrains Mono Nerd Regular")
+config.font_size = 13.5
+
+config.window_padding = {
+  left = 6,
+  right = 2,
+  top = 2,
+  bottom = 0,
+}
+
+config.window_background_opacity = 0.85
+config.prefer_egl = true
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+config.window_decorations = "NONE"
+
+-- tmux
+config.leader = { key = " ", mods = "ALT", timeout_milliseconds = 2000 }
+config.keys = {
+    {
+        mods = "LEADER",
+        key = "c",
+        action = wezterm.action.SpawnTab "CurrentPaneDomain",
+    },
+    {
+        mods = "LEADER",
+        key = "x",
+        action = wezterm.action.CloseCurrentPane { confirm = true }
+    },
+    {
+        mods = "LEADER",
+        key = "p",
+        action = wezterm.action.ActivateTabRelative(-1)
+    },
+    {
+        mods = "LEADER",
+        key = "n",
+        action = wezterm.action.ActivateTabRelative(1)
+    },
+    {
+        mods = "LEADER",
+        key = "|",
+        action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "-",
+        action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" }
+    },
+    {
+        mods = "LEADER",
+        key = "h",
+        action = wezterm.action.ActivatePaneDirection "Left"
+    },
+    {
+        mods = "LEADER",
+        key = "j",
+        action = wezterm.action.ActivatePaneDirection "Down"
+    },
+    {
+        mods = "LEADER",
+        key = "k",
+        action = wezterm.action.ActivatePaneDirection "Up"
+    },
+    {
+        mods = "LEADER",
+        key = "l",
+        action = wezterm.action.ActivatePaneDirection "Right"
+    },
+    {
+        mods = "LEADER",
+        key = "LeftArrow",
+        action = wezterm.action.AdjustPaneSize { "Left", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "RightArrow",
+        action = wezterm.action.AdjustPaneSize { "Right", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "DownArrow",
+        action = wezterm.action.AdjustPaneSize { "Down", 5 }
+    },
+    {
+        mods = "LEADER",
+        key = "UpArrow",
+        action = wezterm.action.AdjustPaneSize { "Up", 5 }
+    },
+}
+
+for i = 0, 9 do
+    -- leader + number to activate that tab
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "ALT",
+        action = wezterm.action.ActivateTab(i - 1),
+    })
+end
+
+-- For example, changing the color scheme:
+config.color_scheme = "Cloud (terminal.sexy)"
+config.colors = {
+	-- background = '#3b224c',
+	-- background = "#181616", -- vague.nvim bg
+	-- background = "#080808", -- almost black
+	-- background = "#0c0b0f", -- dark purple
+	-- background = "#020202", -- dark purple
+	background = "#15151c", -- dark blue
+	-- background = "#17151c", -- brighter purple
+	-- background = "#16141a",
+	-- background = "#0e0e12", -- bright washed lavendar
+	-- background = 'rgba(59, 34, 76, 100%)',
+	cursor_border = "#ffffff",
+	-- cursor_fg = "#281733",
+	cursor_bg = "#ffffff",
+	-- selection_fg = '#281733',
+
+	tab_bar = {
+		background = "#0c0b0f",
+		-- background = "rgba(0, 0, 0, 0%)",
+		active_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#51b5e8",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+		inactive_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#f8f2f5",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+
+		new_tab = {
+			-- bg_color = "rgba(59, 34, 76, 50%)",
+			bg_color = "#0c0b0f",
+			fg_color = "white",
+		},
+	},
+}
+
+-- config.window_frame = {
+-- 	font = wezterm.font({ family = "Iosevka Custom", weight = "Regular" }),
+-- 	active_titlebar_bg = "#0c0b0f",
+-- 	-- active_titlebar_bg = "#181616",
+-- }
+
+-- tab bar
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.tab_and_split_indices_are_zero_based = true
+
+-- tmux status
+wezterm.on("update-right-status", function(window, _)
+    local SOLID_LEFT_ARROW = ""
+    local ARROW_FOREGROUND = { Foreground = { Color = "#51b5e8" } }
+    local prefix = ""
+
+    if window:leader_is_active() then
+        prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+        SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+    end
+
+    if window:active_tab():tab_id() ~= 0 then
+        ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+    end -- arrow color based on if tab is first pane
+
+    window:set_left_status(wezterm.format {
+        { Background = { Color = "#b7bdf8" } },
+        { Text = prefix },
+        ARROW_FOREGROUND,
+        { Text = SOLID_LEFT_ARROW }
+    })
+end)
+
+-- and finally, return the configuration to wezterm
+return config
